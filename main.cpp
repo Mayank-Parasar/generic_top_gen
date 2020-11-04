13d1f8192eaa6f0054943e8a539421cd9fe9cb69
Mayank Parasar <mayankparasar@gmail.com>
2020-11-03 21:28:01 -0600

    added framework code to the tool

--- main.cpp
+++ main.cpp
@@ -7,10 +7,19 @@ int main(int argc, char const *argv[])
     std::boolalpha;
 
 	cout << "Hello world" << endl;
-	cout << "num-nodes: " << argv[1];
-	uint16_t num_nodes = atoi(argv[1]);
-	cout << "num-links: " << argv[2];
-    uint32_t num_links = atoi(argv[2]);
+
+    char const *buffer;
+    buffer = argv[1];
+
+    uint16_t num_nodes = strtoul(argv[1], nullptr, 10);
+    cout << "num-nodes: " << num_nodes << endl;
+    uint32_t num_links = strtoul(argv[2], nullptr, 10);
+    cout << "num-links: " << num_links << endl;
+
+    if(num_nodes > num_links) {
+        cout << "Number of nodes are greater than number of links, " \
+                "therefore a SCC topology is not possible" << endl;
+    }
 
 	Topology *topo = new Topology(num_nodes, num_links);
 //    topo->set_params()
