bff3e3c5a8b7b44ebe0d614dc6cf2bdefa4913e1
Mayank Parasar <mayankparasar@gmail.com>
2020-11-10 18:54:37 -0600

    In progress: adding file handling support in the tool for storing many matrices of different shapes and  sizes

--- file_handler.h
+++ file_handler.h
@@ -6,7 +6,44 @@
 #define GENERIC_TOPO_GEN_FILE_HANDLER_H
 #include <fstream>
 #include <iostream>
+#include <vector>
 
+/* [1]: This code is for reference only:
+ * #include <iostream>
+#include <vector>
+
+std::vector<std::vector<std::vector<int>>*> mat_ptr;
+
+int main(int argc, char* argv[]) {
+
+    for(int mat_id = 2; mat_id < argc; mat_id++)
+    {
+        std::vector<std::vector<int>>* tmp;
+        tmp = new std::vector<std::vector<int>> (atoi(argv[mat_id]),
+                std::vector<int> (atoi(argv[mat_id]), atoi(argv[mat_id])));
+
+        mat_ptr.push_back(tmp);
+    }
+
+    std::cout << "Printing the Matrices.." << std::endl;
+    std::cout << "Number of Matrices populated: " << mat_ptr.size() <<
+    std::endl;
+    for (int mat_id = 0; mat_id < mat_ptr.size(); ++mat_id) {
+        std::cout << "Printing Matrix: " << mat_id << std::endl;
+        for(auto i : *mat_ptr[mat_id]) {
+            for(auto k : i) {
+                std::cout << k << " ";
+            }
+            std::cout << std::endl;
+        }
+    }
+
+    return 0;
+}
+ * */
+
+
+using namespace  std;
 // THis file contains all the data structure related to
 // reading the file.
 class FileHandler {
@@ -17,6 +54,11 @@ private:
     // it should contain the traffic for each application..
     // a new object is created with applicaiton name and
     // initializes that
+public:
+    // This is a vector of pointers to matrices of different
+    // shape and sizes. Refer to the code in comment for usage[1].
+    // This datavstructure will be populated by reading from file
+    std::vector<std::vector<std::vector<int>>*> mat_ptr;
 };
 
 
